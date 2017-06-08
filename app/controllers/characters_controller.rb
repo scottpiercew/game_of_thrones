ef94bbb4ea0a4ddb5e8908e37888ec7894c2140d
Scott Pierce <scottpiercew@gmail.com>
2017-06-08 10:25:32 -0400

    can now add new characters

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -5,4 +5,23 @@ class CharactersController < ApplicationController
     @houses = House.all
   end
 
+  def show
+    @character = Character.find(params[:id])
+  end
+
+  def new
+    @house = House.find(params[:house_id])
+    @character = Character.new
+  end
+
+  def create
+    @house = House.find(params[:house_id])
+    @character = @house.characters.create(character_params)
+    redirect_to houses_path
+  end
+
+  private
+  def character_params
+    params.require(:character).permit(:name, :title, :img_url, :house_id)
+  end
 end
--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -8,4 +8,18 @@ class HousesController < ApplicationController
   def show
     @house = House.find(params[:id])
   end
+
+  def new
+    @house = House.new
+  end
+
+  def create
+    @house = House.create!(house_params)
+    redirect_to house_path(@house)
+  end
+
+  private
+  def house_params
+    params.require(:house).permit(:name, :words, :region, :img_url)
+  end
 end
